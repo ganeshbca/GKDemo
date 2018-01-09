618289992f50180000cf30adc8f38d1620ca293c
ganesh <ganesh.kumar@widas.in>
2018-01-09 17:51:01 +0530

    first commit

---
+++ GKDemo/GKDemo.h
@@ -0,0 +1,19 @@
+//
+//  GKDemo.h
+//  GKDemo
+//
+//  Created by ganesh on 09/01/18.
+//  Copyright © 2018 Cidaas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GKDemo.
+FOUNDATION_EXPORT double GKDemoVersionNumber;
+
+//! Project version string for GKDemo.
+FOUNDATION_EXPORT const unsigned char GKDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKDemo/PublicHeader.h>
+
+
---
+++ GKDemo/GKLog.swift
@@ -0,0 +1,14 @@
+//
+//  GKLog.swift
+//  GKDemo
+//
+//  Created by ganesh on 09/01/18.
+//  Copyright © 2018 Cidaas. All rights reserved.
+//
+
+import Foundation
+public class GKLog {
+    public class func print(message : String) {
+        print(message: message)
+    }
+}
